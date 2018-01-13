c9ecd22213e090900b2c7c0a05b42209d4cd0cfc
Akash Kava <support@neurospeech.com>
2018-01-13 16:17:33 +0530

    Added initial setup

---
+++ Xamarin.FileUploader/FileUploaderService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.FileUploader
+{
+    public partial class FileUploaderService
+    {
+
+        private static FileUploaderService _Instance = null;
+        public static FileUploaderService Instance => 
+            (_Instance ?? (_Instance = Xamarin.Forms.DependencyService.Get<FileUploaderService>()));
+
+
+
+
+
+    }
+}
