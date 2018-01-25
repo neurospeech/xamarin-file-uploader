d50060905a60a58958e8d5d154cf80dd0c5f1368
Akash Kava <support@neurospeech.com>
2018-01-26 01:30:13 +0530

    wip

--- XamarinFileUploader.Droid/BackgroundUploadService.cs
+++ XamarinFileUploader.Droid/BackgroundUploadService.cs
@@ -1,6 +1,7 @@
 ﻿using Android.App;
 using Android.Content;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -35,9 +36,9 @@ namespace XamarinFileUploader
 
 
             while (true) {
-                
-                var pending = FileUploaderService.Instance.Storage.Get().FirstOrDefault(x => x.ResponseCode == 0);
-                if (pending == null)
+
+                var pending = FileUploaderService.Instance.Storage.Get().Take(4).ToList();
+                if (!pending.Any())
                 {
                     FileUploaderService.Instance.ReportPendingStatus().Wait();
                     break;
@@ -61,55 +62,70 @@ namespace XamarinFileUploader
 
         }
 
-        private async Task ProcessAsync(FileUploadRequest pending)
+        private async Task ProcessAsync(IEnumerable<FileUploadRequest> pendings)
         {
             var client = FileUploaderService.Instance.Receiver.GetHttpClient();
 
-            var msg = new HttpRequestMessage(new HttpMethod(pending.Method.ToUpper()), pending.Url);
+            var tasks = pendings.Select(async pending =>
+            {
 
-            using (var input = System.IO.File.OpenRead(pending.FilePath)) {
-                var body = new StreamContent(input);
-                body.Headers.ContentType = System.Net.Http.Headers.MediaTypeHeaderValue.Parse(pending.ContentType);
+                var msg = new HttpRequestMessage(new HttpMethod(pending.Method.ToUpper()), pending.Url);
 
-                var fileInfo = new System.IO.FileInfo(pending.FilePath);
+                using (var input = System.IO.File.OpenRead(pending.FilePath))
+                {
+                    var body = new StreamContent(input);
+                    body.Headers.ContentType = System.Net.Http.Headers.MediaTypeHeaderValue.Parse(pending.ContentType);
 
-                body.Headers.ContentLength = fileInfo.Length;
+                    var fileInfo = new System.IO.FileInfo(pending.FilePath);
 
-                if (pending.Headers != null) {
-                    foreach (var h in pending.Headers)
+                    body.Headers.ContentLength = fileInfo.Length;
+
+                    if (pending.Headers != null)
                     {
-                        body.Headers.TryAddWithoutValidation(h.Key, h.Value);
-                    }
-                }
-
-                msg.Content = new ProgressableStreamContent(body, (n,t) => {
-                    try {
-                        pending.TotalBytes = (int)t;
-                        pending.TotalSent = (int)n;
-                        FileUploaderService.Instance.ReportProgress(pending);
-                    } catch (Exception ex) {
-                        FileUploaderService.Instance.ReportFatalError(ex);
-                    }
-                });
-
-                System.Diagnostics.Debug.WriteLine($"Starting upload for {pending.Identifier}");
-
-                using (var response = await client.SendAsync(msg, HttpCompletionOption.ResponseHeadersRead)) {
-                    using (var s = await response.Content.ReadAsStreamAsync()) {
-
-                        pending.ResponseContentType = response.Content.Headers.ContentType.ToString();
-
-                        pending.ResponseCode = (int)response.StatusCode;
-
-                        using (var outs = System.IO.File.OpenWrite(pending.ResponseFilePath)) {
-                            await s.CopyToAsync(outs);
+                        foreach (var h in pending.Headers)
+                        {
+                            body.Headers.TryAddWithoutValidation(h.Key, h.Value);
                         }
-
                     }
-                }
 
-                System.Diagnostics.Debug.WriteLine($"Finished upload for {pending.Identifier}");
-            }
+                    msg.Content = new ProgressableStreamContent(body, (n, t) =>
+                    {
+                        try
+                        {
+                            pending.TotalBytes = (int)t;
+                            pending.TotalSent = (int)n;
+                            FileUploaderService.Instance.ReportProgress(pending);
+                        }
+                        catch (Exception ex)
+                        {
+                            FileUploaderService.Instance.ReportFatalError(ex);
+                        }
+                    });
+
+                    System.Diagnostics.Debug.WriteLine($"Starting upload for {pending.Identifier}");
+
+                    using (var response = await client.SendAsync(msg, HttpCompletionOption.ResponseHeadersRead))
+                    {
+                        using (var s = await response.Content.ReadAsStreamAsync())
+                        {
+
+                            pending.ResponseContentType = response.Content.Headers.ContentType.ToString();
+
+                            pending.ResponseCode = (int)response.StatusCode;
+
+                            using (var outs = System.IO.File.OpenWrite(pending.ResponseFilePath))
+                            {
+                                await s.CopyToAsync(outs);
+                            }
+
+                        }
+                    }
+
+                    System.Diagnostics.Debug.WriteLine($"Finished upload for {pending.Identifier}");
+                }
+            });
+
+            await Task.WhenAll(tasks);
 
             await FileUploaderService.Instance.ReportPendingStatus();
 
