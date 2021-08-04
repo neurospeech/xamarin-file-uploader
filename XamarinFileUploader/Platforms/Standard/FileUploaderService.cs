63a524aacf95f6c9d5b191ae59f69e083e6f732e
Akash Kava <ackava@gmail.com>
2021-08-04 14:44:34 +0530

    Added Fake implimentor

---
+++ XamarinFileUploader/Platforms/Standard/FileUploaderService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using XamarinFileUploader;
+
+[assembly: Xamarin.Forms.Dependency(typeof(FileUploaderServiceImpl))]
+
+namespace XamarinFileUploader
+{
+    public class FileUploaderServiceImpl : FileUploaderService
+    {
+        protected override void OnCancel(FileUploadRequest r)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void OnStarted()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void StartUploadInternal(FileUploadRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
